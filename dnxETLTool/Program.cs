9cef37fa51b822ceaac8465be9ab2407994eeab8
nteague <nteague@bookmasters.com>
2015-09-29 17:39:53 -0400

    beginning EF ETL

---
+++ dnxETLTool/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+
+namespace dnxEntityETLTool
+{
+    public class Program
+    {
+        internal readonly IApplicationEnvironment aenv;
+
+        internal readonly IServiceCollection svc;
+
+        public IServiceProvider ServBot { get; set; }
+
+        internal readonly IConfigurationBuilder bldr;
+
+        public IConfiguration Config { get { return bldr?.Build(); } }
+
+        public Program(IApplicationEnvironment env, IServiceCollection services)
+        {
+            bldr = new ConfigurationBuilder(env.ApplicationBasePath).AddJsonFile("config.json");
+
+            bldr.AddEnvironmentVariables("bETL");
+
+            ConfigureServices();
+
+            ServBot = svc.BuildServiceProvider();
+
+        }
+        public void Main(string[] args)
+        {
+            bldr.AddCommandLine(args);
+        }
+
+        public void ConfigureServices()
+        {
+            // Add my services
+            svc.AddEntityFramework().AddNpgsql();
+
+            svc.AddEntityFramework().AddSqlServer();
+            
+        }
+    }
+}
